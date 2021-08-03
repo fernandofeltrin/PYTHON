e96ec15c5a58899ec965c89711be61f33ff63e09
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-03 16:46:05 -0300

    Create Sintaxe não permitida ou não recomendada.py

---
+++ Variáveis/Sintaxe não permitida ou não recomendada.py
@@ -0,0 +1,19 @@
+# Formas de declarar uma variável que podem gerar erros de interpretação
+
+Nome              Nome de variável iniciando em letra maiúscula
+
+NOME              Nome de variável escrita totalmente em letras maiúsculas*
+
+8dados            Nome de variável iniciando com números
+
+_numero           Nome de variável iniciando com caractere especial
+
+minha variavel    Nome de variável com espaços
+
+número = 12345    Nome de variável contendo acentos
+
+(variavel2)       Nome de variável encapsulado em chaves, parênteses ou colchetes
+
+# *Uma variável com nome escrito com letras totalmente maiúsculas por
+# convenção pode ser tratado como uma constante, embora em Python não
+# existam constantes.
