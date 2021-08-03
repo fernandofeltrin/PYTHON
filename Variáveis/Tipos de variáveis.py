afe4a20c323d087568bfab7aead52dda96b01f1a
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-03 16:42:11 -0300

    Create Tipos de variáveis.py

---
+++ Variáveis/Tipos de variáveis.py
@@ -0,0 +1,32 @@
+var_int = 12
+# Número inteiro
+
+var_float = 15.3
+# Número com casas decimais
+
+var_string = 'Conjunto de caracteres alfanuméricos'
+# Incluindo símbolos, caracteres e/ou algarismos especiais
+
+var_lista = [1, 2, 'Paulo', 'Maria', True]
+# Lista de dados/valores
+
+var_dicionario = {'nome':'Fernando', 'idade':31}
+# Conjunto de dados:valores
+
+var_tupla = ('Ana', 'Fernando', 3)
+# Lista de dados/valores porém imutável
+
+var_set = {1999, 2000, 2001, 2002, 2003, 2004, 2005}
+# Conjunto de dados numéricos
+
+var_bool = True
+# Booleano / Binário (0 ou 1)
+
+var_comentario = """Um comentário atribuído a uma
+                  variável deixa de ser apenas
+                  um comentário, vira um texto
+                  que pode ser incorporado ao
+                  código..."""
+
+# Os tipos de variáveis como listas, tuplas, dicionários, etc comportam
+# múltiplos dados em sua composição, suportando dados de qualquer tipo.
