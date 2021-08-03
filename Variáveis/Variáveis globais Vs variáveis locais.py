4d28b7631cd904873ed132dfc1ebfa897fc8708b
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-03 16:46:46 -0300

    Create Variáveis globais Vs variáveis locais.py

---
+++ Variáveis/Variáveis globais Vs variáveis locais.py
@@ -0,0 +1,7 @@
+num1 = 19
+# Escopo global, visível e iterável por qualquer estrutura do corpo do código.
+
+if num1 > 100:
+  novo_num = num1
+  # novo_num é acessível e iterável apenas dentro desse bloco de código.
+  print(novo_num)
