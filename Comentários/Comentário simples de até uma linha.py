81918dc716cfa641ab9be5be54d2535507b251ef
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-03 16:40:33 -0300

    Create Comentário simples de até uma linha.py

---
+++ Comentários/Comentário simples de até uma linha.py
@@ -0,0 +1,7 @@
+# Comentário simples de até uma linha.
+
+##################################################
+
+# Embora possa acarretar perda de performance
+# para o código, é perfeitamente possível comentar
+# linha por linha individualmente.
