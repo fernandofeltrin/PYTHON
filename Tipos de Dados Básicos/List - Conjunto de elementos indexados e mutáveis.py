de1787f580ca261182bc18693b2953078e28db66
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-03 16:35:41 -0300

    Create List - Conjunto de elementos indexados e mutáveis.py

---
+++ Tipos de Dados Básicos/List - Conjunto de elementos indexados e mutáveis.py
@@ -0,0 +1,11 @@
+# Basicamente o mesmo tipo de dado array de outras linguagens de programação.
+
+lista = [2, 'Pedro', 15.9]
+# Cada elemento possui um número de índice, começando em 0.
+
+lista2 = list([1, 'Maria', 19.99])
+
+print(lista)
+print(type(lista))
+
+print(lista2)
