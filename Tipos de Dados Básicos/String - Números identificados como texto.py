12ac8528b36b907bac799263ef6eb750bd5a567d
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-03 16:10:09 -0300

    Create String - Números identificados como texto.py

---
+++ Tipos de Dados Básicos/String - Números identificados como texto.py
@@ -0,0 +1,6 @@
+codigo1 = '887237'
+# Apesar de composto apenas por números, o atributo da variável codigo1 por
+# conta de sua sintaxe é um texto, tipo de dado string
+
+print(codigo1)
+print(type(codigo1))
