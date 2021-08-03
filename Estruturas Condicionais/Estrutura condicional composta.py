de31c51febf4457fd96fdb073728423fcbc6e32e
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-03 16:59:16 -0300

    Create Estrutura condicional composta.py

---
+++ Estruturas Condicionais/Estrutura condicional composta.py
@@ -0,0 +1,9 @@
+nome = input('Digite o seu nome:')
+
+if nome == 'Fernando':
+  print('Bem vindo de volta Fernando!!!')
+if nome == 'Maria':
+  print('Bem vinda de volta Maria!!!')
+
+# Sendo a primeira proposição verdadeira, a execução desse bloco
+# de código é encerrada.
