fc8bee34de405072c60d4b7d0e1a4be37bfc75d3
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-03 16:10:36 -0300

    Create Tupla - Conjunto de elementos imutável.py

---
+++ Tipos de Dados Básicos/Tupla - Conjunto de elementos imutável.py
@@ -0,0 +1,7 @@
+# Método simples para guardar mais de um dado/valor atribuído a uma variável.
+
+tupla = ('Ana', 'Fernando', 3, 19.90)
+# Cada elemento declarado conforme sua sintaxe.
+
+print(tupla)
+print(type(tupla))
