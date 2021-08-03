e75fe54d7a9c58af9b9005940f96ff6d99296cb7
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-03 16:09:09 -0300

    Create String - Texto composto por qualquer caractere.py

---
+++ Tipos de Dados Básicos/String - Texto composto por qualquer caractere.py
@@ -0,0 +1,17 @@
+palavra1 = 'palavra'
+# Todo conteúdo contido entre aspas é lido como texto pelo interpretador
+
+palavra2 = "marca d'água"
+# Uso de aspas duplas para não gerar conflito com o apóstrofo '
+
+frase1 = 'Uma frase qualquer'
+
+frase3 = 'Ela tinha 8 anos'
+
+frase3 = str('Outra frase qualquer')
+
+print(palavra1)
+print(palavra2)
+
+print(frase3)
+print(type(frase3))
