662646799d01172763c76b8aa778ef3670962588
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-03 16:47:50 -0300

    Create Tornando números extensos visualmente melhor identificáveis.py

---
+++ Variáveis/Tornando números extensos visualmente melhor identificáveis.py
@@ -0,0 +1,15 @@
+num1 = 1000000000000000000
+# Difícil de identificar
+
+num1 = 1_000_000_000_000_000_000
+# Melhor identificável, um quintilhão 
+# O marcador " _ " não afeta a leitura do valor
+
+print(num1)
+
+# Tornando o marcador visível no retorno
+
+num1 = 1_000_000_000_000_000_000
+
+print(f'{num1:,}')
+
