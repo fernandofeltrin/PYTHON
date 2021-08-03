ed954cfe1dc2e2b7c586e735c306abdf7a270e24
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-03 16:45:07 -0300

    Create Operações entre variáveis.py

---
+++ Variáveis/Operações entre variáveis.py
@@ -0,0 +1,16 @@
+a = 10
+b = 5.2
+
+print(a + b)
+# Soma simples dos conteúdos das variáveis a e b
+
+
+##################################################
+
+a = 10
+b = 5.2
+
+c = a + b
+# Variável c recebe o resultado da soma dos conteúdos das variáveis a e b
+
+print(c)
