eb2e9e429adcc121dd2c1afd923e0d984c58fe0b
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-03 18:30:11 -0300

    Create Iteráveis.py

---
+++ Geradores e Iteradores/Iteráveis.py
@@ -0,0 +1,12 @@
+# Um objeto iterável é aquele com que você consegue interagir 
+# com cada elemento de sua composição, ex cada letra de uma string
+
+lista = [1,2,3,4]
+nome = 'Fernando'
+
+for i in lista:
+  print(i)
+  
+for j in nome:
+  print(j)
+  
