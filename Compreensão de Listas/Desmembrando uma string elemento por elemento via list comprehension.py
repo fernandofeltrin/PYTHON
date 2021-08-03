8849f81c53650b81f9cfba9a70473ad943a93d9c
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-03 20:04:30 -0300

    Delete Compreensão de Listas directory

--- Compreensão de Listas/Desmembrando uma string elemento por elemento via list comprehension.py
+++
@@ -1,4 +0,0 @@
-string = '12345678901234567890'
-comprehension = [letra for letra in string]
-
-print(comprehension)
--- Compreensão de Listas/Fazendo operações com elementos de uma lista usando list comprehension.py
+++
@@ -1,7 +0,0 @@
-lista1 = [1,2,3,4,5,6]
-lista2 = [i * 2 for i in lista1]
-
-# Variável temporária i que percorre cada elemento da lista1 e multiplica por 2
-
-print(lista1)
-print(lista2)
