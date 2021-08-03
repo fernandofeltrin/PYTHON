a75d1854b0ccb09a6b963fcd1937f22990f2a6bf
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-03 20:05:02 -0300

    Delete Estruturas Condicionais directory

--- Estruturas Condicionais/If, else - Estrutura condicional com dois possíveis desfechos.py
+++
@@ -1,9 +0,0 @@
-num = 51
-
-if num < 50:
-  print('Menor que 50')
-else:
-  print('Maior que 50')
-
-# Se o valor de num for menor que 50, exiba em tela 'Menor que 50',
-# caso contrário, exiba em tela 'Maior que 50'.
