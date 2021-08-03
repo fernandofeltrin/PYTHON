5762a802435e9f62ada1a2f3a16ad088d1c8cc3b
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-03 20:07:29 -0300

    Delete Tipos de Dados Básicos directory

--- Tipos de Dados Básicos/Dict - Dicionários, conjunto de elementos baseados em chave e valor.py
+++
@@ -1,5 +0,0 @@
-dicionario = {'Nome':'Fernando', 'Idade':32}
-# Na primeira entrada, 'Nome' é a chave e 'Fernando' é o valor.
-
-print(dicionario)
-print(type(dicionario))
--- Tipos de Dados Básicos/Tupla - Conjunto de elementos imutável.py
+++
@@ -1,7 +0,0 @@
-# Método simples para guardar mais de um dado/valor atribuído a uma variável.
-
-tupla = ('Ana', 'Fernando', 3, 19.90)
-# Cada elemento declarado conforme sua sintaxe.
-
-print(tupla)
-print(type(tupla))
