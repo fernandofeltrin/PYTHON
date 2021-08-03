cfb6a2b5fad8f7f22181c578725a08bf4de71448
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-03 20:05:07 -0300

    Delete Função input( ) directory

--- Função input( )/Função de entrada input( ).py
+++
@@ -1,7 +0,0 @@
-nome = input('Digite o seu nome: ')
-
-print(nome)
-
-# Função de entrada, interage com o usuário fazendo com que o mesmo
-# insira dados. Esta função por sua vez deve ser associada a uma variável.
-# A mesma só termina sua função no momento que o usuário aperta o botão ENTER.
