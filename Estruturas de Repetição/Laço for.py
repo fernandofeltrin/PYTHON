36c35e5fb55ae11e0e073e9d3f00bd9fd5a0a698
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-03 17:01:45 -0300

    Create Laço for.py

---
+++ Estruturas de Repetição/Laço for.py
@@ -0,0 +1,9 @@
+# Laço de repetição simples
+
+compras = ['Arroz', 'Feijão', 'Massa', 'Carne', 'Pão']
+
+for i in compras:
+  print(i)
+
+# A variável temporária i a cada ciclo irá ler um dos elementos da lista
+# compras, sendo que a cada ciclo lê e retorna o elemento subsequente.
