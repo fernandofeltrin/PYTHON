f56d69a6bd200ecc93b107fdf80d9c956aa13352
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-03 20:06:32 -0300

    Delete Interpolação directory

--- Interpolação/Operações dentro de máscaras de substituição.py
+++
@@ -1,7 +0,0 @@
-nome = 'Maria'
-idade = 30
-
-print(f'{nome} tem 3 vezes minha idade, ela tem {3 * idade} anos!!!')
-
-# Dentro de uma máscara de substituição é possivel realizar qualquer tipo
-# de operação lógica ou aritmética, além de funções 
--- Interpolação/Usando de máscaras de substituição.py
+++
@@ -1,7 +0,0 @@
-nome = 'Maria'
-exp = 33
-
-print(f'Olá {nome}, queremos te parabenizar por seus {exp} anos em nossa empresa.')
-
-# Máscaras de substituição serão espaços alocados na própria string para
-# instanciar neste espaço o conteúdo de algum objeto qualquer
