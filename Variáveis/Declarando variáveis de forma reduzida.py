be9e7f4f8e14a32b5bedf17ae8aadef21cff5246
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-03 16:47:20 -0300

    Create Declarando variáveis de forma reduzida.py

---
+++ Variáveis/Declarando variáveis de forma reduzida.py
@@ -0,0 +1,15 @@
+# Código usual, prezando pela legibilidade do mesmo
+
+nome = 'Maria'
+idade = 29
+
+print(f'{nome} tem {idade} anos!!!')
+
+#####################################################
+
+# Código reduzido
+
+nome = 'Maria'; idade = 29; print(f'{nome} tem {idade} anos!!!')
+
+# Uso do separador " ; " para incluir na mesma linha várias
+# declarações/expressões, quando necessário.
