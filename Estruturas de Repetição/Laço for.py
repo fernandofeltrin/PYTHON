439085bb8d369366d14dc7d302a5620cd73b85d7
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-03 20:05:18 -0300

    Delete Estruturas de Repetição directory

--- Estruturas de Repetição/Laço for.py
+++
@@ -1,9 +0,0 @@
-# Laço de repetição simples
-
-compras = ['Arroz', 'Feijão', 'Massa', 'Carne', 'Pão']
-
-for i in compras:
-  print(i)
-
-# A variável temporária i a cada ciclo irá ler um dos elementos da lista
-# compras, sendo que a cada ciclo lê e retorna o elemento subsequente.
--- Estruturas de Repetição/While.py
+++
@@ -1,8 +0,0 @@
-# Enquanto determinada condição for verdadeira, o processo se repete.
-
-x = 0
-
-while x <= 5:
-  print(x)
-  x = x + 1
-  
