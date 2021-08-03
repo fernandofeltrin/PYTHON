f093ff895d749510ea9d95e6c97bf7aa1cea9131
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-03 16:37:10 -0300

    Create Bool - Booleano, true ou false.py

---
+++ Tipos de Dados Básicos/Bool - Booleano, true ou false.py
@@ -0,0 +1,7 @@
+condicao1 = True
+# Tipo de dado usado em expressões/proposições lógicas, onde o retorno é True
+# ou False, referente a Verdadeiro ou Falso, válido ou inválido, ligado ou
+# desligado, etc... de acordo com a proposição.
+
+print(condicao1)
+print(type(condicao1))
