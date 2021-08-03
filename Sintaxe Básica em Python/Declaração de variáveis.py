0e2da1379e9cd3da429c8513c76152944908f3ff
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-03 16:06:47 -0300

    Create Declaração de variáveis.py

---
+++ Sintaxe Básica em Python/Declaração de variáveis.py
@@ -0,0 +1,15 @@
+# nome_da_variavel = dado/valor/atributo
+
+numero = 20
+# Nome da variável: numero
+# Dado/valor atribuído: 20
+
+nome = 'Veronica'
+
+variavel = [21, 'Maria', 19.90]
+
+print(numero)
+# Exibe em tela o conteúdo da variável numero
+
+print(nome)
+print(variavel)
