8c8f2153b936548ca7c9738ed4ef63cc62dfb7d2
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-03 16:53:53 -0300

    Create Operador de atribuição.py

---
+++ Operadores de Atribuição/Operador de atribuição.py
@@ -0,0 +1,10 @@
+nome = 'Fernando'
+# = atribui um dado/valor a uma variável
+
+idade = 33
+
+# Símbolo " = " quando usado uma vez é um operador de atribuição,
+# quando usado duas vezes " == " é um operador de igualdade.
+
+print(nome)
+print(idade)
