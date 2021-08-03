d0bea2ae2260f60f7a08e548e3c3560a0a1b0632
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-03 16:43:18 -0300

    Create Declarando uma variável quanto ao seu tipo.py

---
+++ Variáveis/Declarando uma variável quanto ao seu tipo.py
@@ -0,0 +1,17 @@
+ano_nasc = 1987
+# Automaticamente será identificado como do tipo int
+
+ano_nasc2 = '1987'
+# Automaticamente será identificado como do tipo string
+
+ano_nasc = int(1987)
+# Tipo de dado definido manualmente como int, usando do construtor do objeto
+
+ano_nasc2 = str(1987)
+
+
+print(ano_nasc)
+print(type(ano_nasc))
+
+print(ano_nasc2)
+print(type(ano_nasc2))
