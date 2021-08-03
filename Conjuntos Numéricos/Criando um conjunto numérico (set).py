0c3ed061288d3d557cf21f9cced50ace0f241017
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-03 20:04:49 -0300

    Delete Conjuntos Numéricos directory

--- Conjuntos Numéricos/Criando um conjunto numérico (set).py
+++
@@ -1,8 +0,0 @@
-conjunto1 = {5, 10, 15, 20}
-
-print(conjunto1)
-print(type(conjunto1))
-
-# Apesar da notação do conteúdo entre chaves { }, como não existem
-# conjuntos de chaves : valores, o interpretador identifica tal tipo
-# de dado como set
