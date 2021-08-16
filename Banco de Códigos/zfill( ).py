d80b8ce5f416c845f6fc3af41457cd40e3ef6f13
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-16 10:38:17 -0300

    Create zfill( ).py

---
+++ Banco de Códigos/zfill( ).py
@@ -0,0 +1,3 @@
+id_paciente = '63'.zfill(6) # Supondo que por motivos de organização o registros de pacientes seja padronizado em números de 6 dígitos, zfill(6) preenche à esquerda os dígitos faltantes com zeros
+
+print(id_paciente)
