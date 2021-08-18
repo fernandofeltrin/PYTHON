0f237d3736345fb46581768845606b1377c479c6
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-18 13:13:51 -0300

    Create round( ).py

---
+++ Banco de Códigos/round( ).py
@@ -0,0 +1,9 @@
+equacao = ((50 + 25) * 7.2) / 3.8
+
+print(f'O resultado bruto da equação é: {equacao}')
+
+print(f'O resultado arredondado da equação é: {equacao:.2f}')
+# :.2f define quantas casas decimais serão exibidas após a vírgula
+
+print(f'O resultado arredondado da equação é: {round(equacao, 2)}')
+# Função round() parametrizada com a expressão e em seguida com o número de casas após a vírgula
