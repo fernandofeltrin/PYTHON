44b71a58a9c5a837cfe62c39427a0de2c6583dd8
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-18 13:56:18 -0300

    Create repeat( ).py

---
+++ Banco de Códigos/repeat( ).py
@@ -0,0 +1,8 @@
+from itertools import count, cycle, repeat
+
+var1 = 'Python'
+
+for i in repeat(var1, 8):
+    print(i)
+# repeat( ) irá repetir um dado/valor um número específico de vezes definido
+# em seu segundo parâmetro.
