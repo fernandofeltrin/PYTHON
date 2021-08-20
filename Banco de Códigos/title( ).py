713bc1c60ae32d633879c115e7e3a9627c3b790d
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-08-20 12:48:33 -0300

    Create title( ).py

---
+++ Banco de Códigos/title( ).py
@@ -0,0 +1,8 @@
+tema = 'O diagnóstico por imagem como ferramenta para detecção de câncer'
+
+print(tema)
+
+tema = tema.title()
+# title( ) converterá todas letras iniciais da string para letra maiúscula
+
+print(tema)
