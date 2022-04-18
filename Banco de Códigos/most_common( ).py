6fb119931ac8a7b719abfc835863d7024f36dfe1
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2022-04-18 13:50:21 -0300

    Create most_common( ).py

---
+++ Banco de Códigos/most_common( ).py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+texto = 'A Radiologia é a ciência que estuda a anatomia por meio de exames de imagem'
+
+a1, a2, a3, a4, a5, a6 = Counter(texto).most_common(6)
+
+print(f'O elemento mais comum é: "{a2[0]}", repetido {a2[1]} vezes')
+print(f'O elemento mais comum é: "{a3[0]}", repetido {a3[1]} vezes')
+print(f'O elemento mais comum é: "{a4[0]}", repetido {a4[1]} vezes')
+print(f'O elemento mais comum é: "{a5[0]}", repetido {a5[1]} vezes')
+print(f'O elemento mais comum é: "{a6[0]}", repetido {a6[1]} vezes')
