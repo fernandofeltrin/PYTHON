62e1e8c2eeadf53afee5ecf411ab3dd5e5762a0f
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2022-06-17 18:25:21 -0300

    Create np.eye( ).py

---
+++ Banco de Códigos/Numpy/np.eye( ).py
@@ -0,0 +1,5 @@
+# Crie uma array Numpy em formato de matriz transversa / matriz de identidade / matriz diagonal, contendo 10 elementos na diagonal, todos de tipo inteiro
+array = np.eye(N = 10,
+               dtype='int')  
+     
+print(array)
