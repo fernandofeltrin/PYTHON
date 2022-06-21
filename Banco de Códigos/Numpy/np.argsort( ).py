9afbb70b506b37b55910e04dd899f6d7e8e57db3
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2022-06-21 16:10:02 -0300

    Create np.argsort( ).py

---
+++ Banco de Códigos/Numpy/np.argsort( ).py
@@ -0,0 +1,5 @@
+# Uma vez que temos uma array Numpy [2, 1, 6, 4, 5, 3], retorne a referência dos índices que ordenariam de forma crescente os elementos da array
+array = np.array([2, 1, 6, 4, 5, 3])
+print(array)
+ 
+print(np.argsort(array))
