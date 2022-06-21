874d9e6a65b84d5bc6118a48103a145f77dd1c22
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2022-06-21 15:36:59 -0300

    Create np.sqrt( ).py

---
+++ Banco de Códigos/Numpy/np.sqrt( ).py
@@ -0,0 +1,5 @@
+# Fornecida a array Numpy [[3, 4, 9, 2], [5, 3, 2, 5]], crie uma nova array baseada na raiz quadrada de cada um dos elementos da array de origem.
+A = np.array([[3, 4, 9, 2], [5, 3, 2, 5]])
+B = np.sqrt(A)
+
+print(B)
