a907fce678ac9164fc9dfaa6baded55ee48d915d
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2022-06-21 15:35:11 -0300

    Create np.multiply( ).py

---
+++ Banco de Códigos/Numpy/np.multiply( ).py
@@ -0,0 +1,7 @@
+# Dadas duas arrays Numpy iniciais, crie uma terceira array composta pelo resultado da multiplicação dos elementos das duas anteriores. 
+A = np.array([[3, 4, 9, 2], [5, 3, 2, 5]])
+B = np.array([[4, 3, 2, 5], [6, 3, 1, 6]])
+
+C = np.multiply(A, B)
+ 
+print(C)
